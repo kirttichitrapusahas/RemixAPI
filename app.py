51453f97b5d7c68fd83e8a42e56456d5d6aa6740
kirtichitrapusahas <kirtichitrapu@gmail.com>
2025-04-08 16:36:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def remix():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=8080)
