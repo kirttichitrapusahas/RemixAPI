8640f2ee5eb0d8ebc9868ac829fc30e44b17f639
kirtichitrapusahas <kirtichitrapu@gmail.com>
2025-04-10 10:37:25 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set the public URL here (replace this with your actual RunPod URL)
+export PUBLIC_URL="https://0275-203-57-40-161.ngrok-free.ap"
+
+# Start the Flask app
+python3 app.py
